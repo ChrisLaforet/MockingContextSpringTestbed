066cc92d73f757a751aeab7fea4e4f2c02fc46f6
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-04-17 16:20:53 -0400

    Added spring plumbing

---
+++ src/main/java/com/chrislaforetsoftware/mockingcontextspring/MockingContextSpringApplication.java
@@ -0,0 +1,13 @@
+package com.chrislaforetsoftware.mockingcontextspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockingContextSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockingContextSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chrislaforetsoftware/mockingcontextspring/MockingContextSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrislaforetsoftware.mockingcontextspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockingContextSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
